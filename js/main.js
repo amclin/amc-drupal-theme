6fdba5079717b6427a3980bc3603f5d92f04c135
Anthony McLin <git@anthonymclin.com>
2018-12-04 16:35:17 -0800

    Replace background image element in DOM with image background for #10

--- js/main.js
+++ js/main.js
@@ -1,39 +1,5 @@
 jQuery(document).ready(function($) {
 	/**
-	 * Resize the background image to fully fit the browser window
-	 * without any ratio distortion.
-	 */
-	$(window).resize(function() {
-		var backgroundImage = $('#block-views-random-photo-block img');
-		var windowHeight = $(window).height();
-		var windowWidth = $(window).width();
-		var imageHeight = backgroundImage.attr('height');
-		var imageWidth = backgroundImage.attr('width');
-		var browserRatio	= windowWidth/windowHeight;
-		var imageRatio 		= imageWidth/imageHeight;
-		
-		//Resize the image appropriately for height or width
-		if(browserRatio <= imageRatio) {
-			//Browser proportions are narrower than the image proportions
-			backgroundImage.css({
-				'height': 		windowHeight,
-				'width': 		windowHeight * imageRatio,
-				'margin-top':	0,
-				'margin-left':	(-(windowWidth - (windowHeight * imageRatio))/-2)	
-			});
-
-		} else {
-			//Browser proportions are wider than the image proportions
-			backgroundImage.css({
-				'width':		windowWidth,
-				'height':		windowWidth/imageRatio,
-				'margin-top':	(((windowWidth/imageRatio) - windowHeight)/-2),
-				'margin-left':	0
-			});
-		}
-	});
-	
-	/**
 	 * Force the superfish menu to align top instead of drop down
 	 */
     $('#menu .menu li > ul').each(function() {
