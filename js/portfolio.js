6b8cb5fda62803f29d5c94fe1fcf919a8ac7ea13
Anthony McLin <git@anthonymclin.com>
2018-12-04 16:49:51 -0800

    fix JSON syntax

--- js/portfolio.js
+++ js/portfolio.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function($) {
          */
         var setBackground = function(url) {
             $('.region-background').css({
-                'background-image': `url(${url})`,
+                "background-image": "`url(${url})`",
             });
         }
 
