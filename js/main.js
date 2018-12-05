996926e4ad5de500d135f2bcafa70d3c6c2e1ed4
Anthony McLin <git@anthonymclin.com>
2018-12-04 16:37:59 -0800

    Leverage CSS based positioning for portfolio image for #10. Reduce DOM queries when generating portfolio.

--- js/main.js
+++ js/main.js
@@ -6,32 +6,7 @@ jQuery(document).ready(function($) {
     	var position = $(this).parent().position();
         $(this).css('top', 4-(position.top));    
     });
-    
-    /**
-     * Build the portfolio gallery
-     */
-    if($('.view-id-photo_gallery').length == 1) {
-    	//Setup buttons
-    	var photos = $('.view-id-photo_gallery img');
-    	$('#filler').after("<div id='slide_buttons'><ul>");
-    	photos.each(function(idx,el) {
-    		$('#slide_buttons ul').append("<li><a href='"+$(el).attr('src')+"' title='"+$(el).attr('alt')+"'>"+(idx+1)+"</a></li>");
-    	});
-    	//Handle image swap on click
-    	var backgroundImage = $('#block-views-random-photo-block img');
-    	var slideCounters = $('#slide_buttons li');
-    	slideCounters.click(function(ev) {
-    		ev.preventDefault();
-    		slideCounters.removeClass('active');
-    		$(this).addClass('active');
-    		backgroundImage.attr('src',$(this).find('a').attr('href'));
-    		$(window).resize();
-    	});
-    	
-    	//Trigger first slide
-    	slideCounters[0].click();
-    }
-    
+
     /**
      * Trigger an initial resize
      */
---
+++ js/portfolio.js
@@ -0,0 +1,44 @@
+/**
+ * Build the portfolio gallery
+ */
+jQuery(document).ready(function($) {
+    if($('.view-id-photo_gallery').length == 1) {
+        /**
+         * Sets the background image of the page
+         * @param {Url} url to the image
+         */
+        var setBackground = function(url) {
+            $('.region-background').css({
+                'background-image': `url(${url})`,
+            });
+        }
+
+    	//Setup buttons
+        var $photos = $('.view-id-photo_gallery img');
+        var $button_list = $('<ul>');
+    	$photos.each(function(idx,el) {
+            $el = $(el);
+            $button_list.append(`
+                <li>
+                    <a href="${$el.attr('src')}"
+                        title="${$el.attr('alt')}">${(idx+1)}</a>
+                </li>
+            `);
+        });
+        $('#filler').after($('<div id="slide_buttons">').append($button_list));
+
+    	//Handle image swap on click
+        var slideCounters = $('#slide_buttons li');
+        slideCounters.on('click', 'a', (ev) => {
+            ev.preventDefault(); // Block clicking on anchors
+            var $a = $(ev.target)
+            var $li = $a.parent();
+    		slideCounters.removeClass('active');
+            $li.addClass('active');
+            setBackground($a.attr('href'));
+    	});
+    	
+    	//Trigger first slide
+    	$('a', slideCounters[0]).click();
+    }
+});
